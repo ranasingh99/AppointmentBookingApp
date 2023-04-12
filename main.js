e073cbbaeb66b237e147fd9c2e7188c48662556e
ranasingh99 <sanketverma852@gmail.com>
2023-04-12 10:59:29 +0530

    localstorage nd coockies

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function Person(firstname,lastname,age,sex){
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.age = age;
-    this.sex = sex;
-    this.getFullName = function(){
-        return `${this.firstname} +${this.lastname}`;
-    }
-}
-const person1= new Person("rana","singh",24,"male");
-console.log(person1.fullName);
+
+//===============COOKIES VS SESSION STORAGE VS LOCAL STORAGE
+
+//LOCALSTORAGE
+//localStorage.setItem('name','Rana');
+//to get the value from localstorage
+//localStorage.getItem('name');
+//to remove from localStorage
+//localStorage.removeItem('name');
+
+// SESSION STORAGE
+
+// sessionStorage.setItem('email','ranasingh@gmail.com');
+// console.log(sessionStorage.getItem('email'));
+// sessionStorage.removeItem('email');
+
+//COOKIES
+document.cookie = "username=John Doe";
+document.cookie = 'name=rana; expires=' + new Date(2023,0,04).toUTCString();
+document.cookie = 'lstname=singh';
